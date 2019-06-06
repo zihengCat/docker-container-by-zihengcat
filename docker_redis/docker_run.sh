029aba0b060de3c42ac0bbd803d1a5f126279bf5
ziheng <zihenglv@gmail.com>
2019-06-07 00:26:37 +0800

    [update]: entrypoint.sh

--- docker_redis/docker_run.sh
+++ docker_redis/docker_run.sh
@@ -1,6 +1,25 @@
 #!/bin/bash
+# ------------------------------
+REDIS_OPTIONS="--loglevel verbose \
+               --bind 0.0.0.0 \
+               --port 6479 \
+               --requirepass foobar123 \
+               --maxclients 10000 \
+               --timeout 0 \
+               --tcp-keepalive 300 \
+               --daemonize no \
+               --supervised no \
+               --pidfile none \
+               --databases 32 \
+               --always-show-logo yes \
+               --appendonly no \
+               --appendfsync no"
+# ------------------------------
 sudo docker run -d -i -t \
                 --env LC_ALL=en_US.UTF-8 \
                 --name redis \
-                -p 0.0.0.0:6379:6379 \
-                ziheng/centos:redis /usr/sbin/init
+                -p 0.0.0.0:6479:6479 \
+                ziheng/centos:redis \
+                ${REDIS_OPTIONS}
+#/usr/sbin/init
+# ------------------------------
--- docker_redis/entrypoint.sh
+++ docker_redis/entrypoint.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
-
+# ------------------------------
 set -e
-
+# ------------------------------
 REDIS_PATH='/opt/redis_5.0.5/bin'
 REDIS_CMD='redis-server'
 #REDIS_CONF='/root/redis.conf'
 REDIS_OPTIONS=''
-
+# ------------------------------
 if [[ "${1}" != '' ]]
 then
     ${REDIS_PATH}/${REDIS_CMD} "${@}"
 fi
+# ------------------------------
